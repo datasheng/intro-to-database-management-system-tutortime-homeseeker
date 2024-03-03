987191a46bc822d5afbd76a25ee529acb1e643e7
Michael Romashov <mromashov@icloud.com>
2024-03-02 21:57:12 -0500

    implement user authentication

--- db-procs/02-insert-data.sql
+++ db-procs/02-insert-data.sql
@@ -1,7 +1,7 @@
 USE bookings_db;
 
 INSERT INTO user (id, first_name, last_name, email, password_hash)
-VALUES (1, 'Michael', 'Romashov', 'mromashov@icloud.com', '');
+VALUES (1, 'Michael', 'Romashov', 'mromashov@icloud.com', '$argon2id$v=19$m=65536,t=2,p=1$/IVjWtWIb7CH7T2F6lyMrg$7ErKzfIG1pkkXIYaM5duTOV9e7tbDdE1AXyESlC4kFU');
 INSERT INTO user (id, first_name, last_name, email, password_hash)
 VALUES (2, 'Joseph', 'Platt', 'jplatt001@citymail.cuny.edu', '');
 INSERT INTO user (id, first_name, last_name, email, password_hash)
