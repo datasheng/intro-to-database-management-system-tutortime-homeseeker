27a40711e8dbcb2d6011192cecc4bdcaf37b9789
Michael Romashov <mromashov@icloud.com>
2024-05-15 20:33:39 -0400

    fix syntax error in procedure

--- db-procs/05-create-procedures.sql
+++ db-procs/05-create-procedures.sql
@@ -4,14 +4,14 @@ DELIMITER //
 
 CREATE PROCEDURE GetBillByUser(IN userId INT)
 BEGIN
-    SELECT (ROUND SUM(amount), 2) AS amount
+    SELECT ROUND(SUM(amount), 2) AS amount
     FROM transaction
     WHERE transaction.payee_id = userId;
 END //
 
 CREATE PROCEDURE GetPaymentByUser(IN userId INT)
 BEGIN
-    SELECT (ROUND SUM(amount), 2) AS amount
+    SELECT ROUND(SUM(amount), 2) AS amount
     FROM transaction
     WHERE transaction.recipient_id = userId;
 END //
