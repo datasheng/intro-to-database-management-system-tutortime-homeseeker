c9cf938077600b078ccd55d8f2b74fab48b969e6
Michael Romashov <mromashov@icloud.com>
2024-05-15 20:29:31 -0400

    fix procedure calls

--- src/app/user/actions.ts
+++ src/app/user/actions.ts
@@ -7,7 +7,12 @@ import {
 	getUpcomingAppointmentsByBroker,
 } from "@/db/homeseeker/appointment";
 import { getPropertiesByUser } from "@/db/homeseeker/property";
-import { getTransactionsByPayee, getTransactionsByRecipient, GetBillByUserID, GetPaymentByUserID } from "@/db/transaction";
+import {
+	getBillByUserID,
+	getPaymentByUserID,
+	getTransactionsByPayee,
+	getTransactionsByRecipient,
+} from "@/db/transaction";
 
 export async function getUserProperties(user_id: number) {
 	return await getPropertiesByUser(user_id);
@@ -34,8 +39,8 @@ export async function getRecipientPayments(user_id: number) {
 }
 
 export async function getAmount(user_id: number) {
-	const bills = GetBillByUserID(user_id);
-	const payments = GetPaymentByUserID(user_id);
+	const bills = getBillByUserID(user_id);
+	const payments = getPaymentByUserID(user_id);
 	return {
 		yourAmount: bills,
 		otherAmount: payments
